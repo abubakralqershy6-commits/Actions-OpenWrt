22c56bc02451af0bf0acf1b1ce1b31ec18c77887
alqershi <abubakralqershy6@gmail.com>
2026-06-07 04:38:41 +0300

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-# تعيين مستودعات حزم ImmortalWrt الرسمية المستقرة
-sed -i 's/^src-git packages.*/src-git packages https:\/\/github.com\/immortalwrt\/packages.git;openwrt-23.05/' feeds.conf.default
-sed -i 's/^src-git luci.*/src-git luci https:\/\/github.com\/immortalwrt\/luci.git;openwrt-23.05/' feeds.conf.default
-sed -i 's/^src-git routing.*/src-git routing https:\/\/github.com\/immortalwrt\/routing.git;openwrt-23.05/' feeds.conf.default
-sed -i 's/^src-git telephony.*/src-git telephony https:\/\/github.com\/immortalwrt\/telephony.git;openwrt-23.05/' feeds.conf.default
+
+# حذف ملف الـ feeds القديم بالكامل لضمان النظافة
+rm -rf feeds.conf.default
+
+# إنشاء ملف feeds جديد خاص بـ ImmortalWrt
+echo "src-git packages https://github.com/immortalwrt/packages.git;openwrt-23.05" >> feeds.conf.default
+echo "src-git luci https://github.com/immortalwrt/luci.git;openwrt-23.05" >> feeds.conf.default
+echo "src-git routing https://github.com/immortalwrt/routing.git;openwrt-23.05" >> feeds.conf.default
+echo "src-git telephony https://github.com/immortalwrt/telephony.git;openwrt-23.05" >> feeds.conf.default
+echo "src-git immortalwrt https://github.com/immortalwrt/immortalwrt.git;openwrt-23.05" >> feeds.conf.default
